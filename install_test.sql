8e1011b10870825590f26de72432d99e3c553a41
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-01-21 09:43:47 +0100

    addf install script for tests (optional)

---
+++ install_test.sql
@@ -0,0 +1,20 @@
+set define off
+set echo off
+spool install_test.log
+
+prompt ================================================================================================================
+prompt install test packages
+prompt ================================================================================================================
+
+@test/package/test_initial_load.pks
+@test/package/test_initial_load.pkb
+@test/package/test_wordle.pks
+@test/package/test_wordle.pkb
+
+
+prompt ================================================================================================================
+prompt execute all utPLSQL tests in current schema
+prompt ================================================================================================================
+
+set serveroutput on size unlimited
+execute ut.run();
