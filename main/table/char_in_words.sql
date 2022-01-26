8fc92b6eeffe5931d98477df3f8bb150b408bb36
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-01-26 08:46:26 +0100

    add unique key to char_in_words table

--- main/table/char_in_words.sql
+++ main/table/char_in_words.sql
@@ -2,5 +2,6 @@ create table char_in_words (
     character    varchar2(1 char) not null constraint chars_in_words_chars_fk references chars,
     word         varchar2(5 char) not null constraint chars_in_words_word_fk references words,
     occurrences  integer          not null,
-                                  constraint char_in_words_pk primary key (character, word)
+                                  constraint char_in_words_pk primary key (character, word),
+                                  constraint char_in_words_uk unique (word, character)
 );
