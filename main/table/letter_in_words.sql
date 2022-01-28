b67e7c023f9f778eeaaa1f5896861580761e5601
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-01-28 09:59:22 +0100

    rename char_in_words to letter_in_words

---
+++ main/table/letter_in_words.sql
@@ -0,0 +1,7 @@
+create table letter_in_words (
+    letter       varchar2(1 char) not null constraint letter_in_words_letters_fk references letters,
+    word         varchar2(5 char) not null constraint letter_in_words_words_fk references words,
+    occurrences  integer          not null,
+                                  constraint letter_in_words_pk primary key (word, letter),
+                                  constraint letter_in_words_uk unique (letter, word)
+);
