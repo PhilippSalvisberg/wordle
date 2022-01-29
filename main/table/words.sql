22cd7b93f9ce9cd6eb93228553bdf5884c1e433b
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-01-29 11:30:05 +0100

    add unique constraints for game_id and game_date

--- main/table/words.sql
+++ main/table/words.sql
@@ -1,5 +1,7 @@
 create table words (
     word       varchar2(5 char) not null constraint words_pk primary key,
     game_id    integer,
-    game_date  date
+    game_date  date,
+                                constraint words_uk1 unique (game_id),
+                                constraint words_uk2 unique (game_date)
 );
