e3ac3d9efc34920a54bdef5c430882e4a81627ec
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-01-29 11:30:36 +0100

    change schema name to wh

--- main/user/create_user.sql
+++ main/user/create_user.sql
@@ -1,21 +1,21 @@
--- example, create user as DBA user
+-- create user wh as DBA user (wh is an acronym for Wordle Helper)
 -- the user must not be named wordle to avoid conflicts with PL/SQL naming resolution
 -- during tests a PLS-00302 is thrown when the user is named wordle (same as package)
 -- it looks like a restriction of anydata.convertcollection
-create user wordle2 identified by wordle2;
+create user wh identified by wh;
 
 -- privileges to install all objects and run tests
-grant connect, resource, create view, unlimited tablespace to wordle2;
+grant connect, resource, unlimited tablespace to wh;
 
 -- privileges to run the debugger
-grant debug connect session, debug any procedure to wordle2;
-grant execute on dbms_debug_jdwp to wordle2;
+grant debug connect session, debug any procedure to wh;
+grant execute on dbms_debug_jdwp to wh;
 begin
     dbms_network_acl_admin.append_host_ace (
     host =>'*',
     ace  => sys.xs$ace_type(
                 privilege_list => sys.xs$name_list('JDWP') ,
-                principal_name => 'wordle2',
+                principal_name => 'wh',
                 principal_type => sys.xs_acl.ptype_db
             )
     );
