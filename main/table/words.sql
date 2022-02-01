4445e9a896a703b7d884a337b0773121f9c29625
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-02-01 23:53:47 +0100

    add check constraints to insure dates without time and more

--- main/table/words.sql
+++ main/table/words.sql
@@ -3,5 +3,10 @@ create table words (
     game_id    integer,
     game_date  date,
                                 constraint words_uk1 unique (game_id),
-                                constraint words_uk2 unique (game_date)
+                                constraint words_uk2 unique (game_date),
+                                constraint words_game_date_without_time_ck 
+                                   check (game_date = trunc(game_date)),
+                                constraint words_game_id_and_game_date_not_null_ck 
+                                   check (game_id is not null and game_date is not null 
+                                          or game_id is null and game_date is null)
 );
