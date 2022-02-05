91bd64a2c2c5b78a49fac487cb9d2e5002fac1da
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2022-02-05 13:10:52 +0100

    format create table statement

--- main/table/letters.sql
+++ main/table/letters.sql
@@ -1,5 +1,12 @@
 create table letters (
-    letter       varchar2(1 char) not null constraint letters_pk primary key,
-    occurrences  integer          not null,
-    is_vowel     integer          generated always as (case when letter in ('a', 'e', 'i', 'o', 'u') then 1 else 0 end) virtual
+   letter       varchar2(1 char) not null constraint letters_pk primary key,
+   occurrences  integer          not null,
+   is_vowel     integer          generated always as (
+                                    case 
+                                       when letter in ('a', 'e', 'i', 'o', 'u') then 
+                                          1 
+                                       else
+                                          0 
+                                     end
+                                 ) virtual
 );
